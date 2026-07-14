66e91c522a4d55dbe0eacc95a6174ecd9f281f09
Sido Santos <yaasirdesantos@gmail.com>
2026-07-15 01:33:37 +0300

    Ongeza PWA install, icons, na vichupo vya search

--- sw.js
+++ sw.js
@@ -1,16 +1,24 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll(['index.html', 'daladala.html', 'manifest.json']);
-    })
+const CACHE_NAME = "daladala-navigator-v2";
+const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
 
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
